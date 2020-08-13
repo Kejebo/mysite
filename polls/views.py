32695ee9815f307201e5f39a17c674fd2e51f7a0
Kejebo <57534905+Kejebo@users.noreply.github.com>
2020-08-13 15:17:07 -0600

    ..

--- polls/views.py
+++ polls/views.py
@@ -21,9 +21,7 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
@@ -37,7 +35,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -45,8 +42,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
